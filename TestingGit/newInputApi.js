a3b5b147f8bb3bcd73d14b7d54b95a0233bf734c
jlmanriqueza <manriquezjoseluis@gmail.com>
2020-04-16 21:17:16 -0400

    segundo round

---
+++ TestingGit/newInputApi.js
@@ -0,0 +1,37 @@
+{
+  "cantOportunidades": 1162,
+  "oportunidades": [
+    {
+      "estado": "EN PROCESO",
+      "uuidOportunidad": "CDN20191121072153390PLAT15426",
+      "canal": "INTERNET_P",
+      "creacion": "JUE 21/11/2019 19:16",
+      "tarea": "",
+      "creador": "lcmorales",
+      "oficina": "446 Banca Virtual",
+      "asiginadoA": "",
+      "montoSolicitado": "0",
+      "rutCliente": "19000058-3",
+      "nombreCliente": "Prueba F Prueba F CASTILLO",
+      "segmentoCliente": null,
+      "recepcionRiesgo": null,
+      "red": null,
+      "sometidoAprobacion": null,
+      "productos": "Cuentas",
+      "solicitudes": null,
+      "idGrupo": null,
+      "decision": null,
+      "nombreGrupo": null,
+      "tipoCliente": "P",
+      "codigoTipoOperacion": null,
+      "canalSolicitud": null,
+      "tipoAutorizacion": null,
+      "accion": null,
+      "rutSolicitante": null,
+      "oficinaOrigen": null,
+      "trxIdOperacion": null,
+      "rutAutorizador": null,
+      "cui": null
+    }
+  ]
+}
